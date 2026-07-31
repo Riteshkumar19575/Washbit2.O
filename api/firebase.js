4b45443175828e48482911f41f955f77e5c754a5
ritesh kumar <riteshkumar19575@gmail.com>
2026-08-01 01:38:16 +0530

    Update firebase.js

--- api/firebase.js
+++ api/firebase.js
@@ -1,13 +1,15 @@
 const admin = require("firebase-admin");
-console.log("PROJECT_ID:", !!process.env.FIREBASE_PROJECT_ID);
-console.log("CLIENT_EMAIL:", !!process.env.FIREBASE_CLIENT_EMAIL);
-console.log("PRIVATE_KEY:", !!process.env.FIREBASE_PRIVATE_KEY);
+
+console.log("PROJECT_ID =", process.env.FIREBASE_PROJECT_ID);
+console.log("CLIENT_EMAIL =", process.env.FIREBASE_CLIENT_EMAIL);
+console.log("PRIVATE_KEY EXISTS =", !!process.env.FIREBASE_PRIVATE_KEY);
+
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      privateKey: (process.env.FIREBASE_PRIVATE_KEY || "").replace(/\\n/g, "\n"),
     }),
   });
 }
